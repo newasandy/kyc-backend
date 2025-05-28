3e1da019ce251bdb8f419ad591b6ab69eb61d6bd
Pallav Niraula <pallavniraula@gmail.com>
2025-05-28 15:15:51 +0545

    Create BaseEntity.java
    Created baseentity

---
+++ src/main/java/org/acme/model/BaseEntity.java
@@ -0,0 +1,21 @@
+package org.acme.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+}
