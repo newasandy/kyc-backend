5833df378516a7a4ab94e1784499978a912e36be
Sandip Rajbhandari <srb.newar.yb0111@gmail.com>
2025-05-29 15:40:59 +0545

    refactor

--- src/main/java/org/acme/dao/daoimpl/CustomerKycDaoImpl.java
+++ src/main/java/org/acme/dao/daoimpl/CustomerKycDaoImpl.java
@@ -1,10 +1,12 @@
 package org.acme.dao.daoimpl;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import org.acme.dao.CustomerKycDao;
 import org.acme.model.CustomerKyc;
 
+@ApplicationScoped
 public class CustomerKycDaoImpl extends BaseDaoImpl<CustomerKyc, Long> implements CustomerKycDao {
 
     public CustomerKycDaoImpl(){
